45fddc4592cba3d21fd4fece4881970beaafef59
sayedmahmoud2 <sayedmahmoud2070@gmail.com>
2024-06-21 15:34:13 -0700

    added c file with if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* Description: printing some text in certin conditions
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
