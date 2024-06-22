4a61bec69241b8c34b0f7a2c61863bf7a4d625c9
sayedmahmoud2 <sayedmahmoud2070@gmail.com>
2024-06-21 18:09:30 -0700

    added 2 files 6-print_numberz.c 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Prints numbers between 0 to 9.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
