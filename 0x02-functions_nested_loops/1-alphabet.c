f8ff254388d92c24a8450a5349fbae0c245dc2c1
root <root@DESKTOP-5OLP95L>
2024-06-26 02:41:33 +0300

    add file to the alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* main- print_alphabet - Prints the alphabet in lowercase.
+*print_alphabet - Prints the alphabet in lowercase.
 *
 * Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
