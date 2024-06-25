31cf9a04f7a22105dd2e39a58e2ed3235db73cb4
root <root@DESKTOP-5OLP95L>
2024-06-26 02:51:01 +0300

    add file to print the alpha 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
