45b010f4b9676b6f9c32ba913503a770e9187b52
root <root@DESKTOP-5OLP95L>
2024-06-26 03:59:20 +0300

    add file to printthe abslute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_abs- print_ - Prints the alphabet in lowercase 10 times.
+*@i: the number input in the function
+* Return: Always 0 (Success)
+*/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
