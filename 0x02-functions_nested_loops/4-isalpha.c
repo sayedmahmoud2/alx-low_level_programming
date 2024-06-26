092f8e075b3123ede27c0bce4b2f1f45ca7178e0
root <root@DESKTOP-5OLP95L>
2024-06-26 03:37:45 +0300

    add file to check the alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isalpha- print_ - Prints the alphabet in lowercase 10 times.
+*@c: checks input function
+* Return: Always 0 (Success)
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
