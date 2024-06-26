0ae6e55310e98227cf863379d9ed9604814a861f
root <root@DESKTOP-5OLP95L>
2024-06-26 03:50:19 +0300

    add file to check the alpha

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_sign- print_ - Prints the alphabet in lowercase 10 times.
+*@n: checks input function
+* Return: Always 0 (Success)
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(49);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+
+}
