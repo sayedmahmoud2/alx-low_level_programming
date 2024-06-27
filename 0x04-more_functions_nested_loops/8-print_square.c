0fb1c699da027ba9e6b5bb7f4823ba6433542e08
root <root@DESKTOP-5OLP95L>
2024-06-28 00:23:00 +0300

    added function to print a ractengal

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
-* print_square - prints hashes squares.
-* @size: size of the square.
-* Return: no return.
-*/
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
 void print_square(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	for (i = 0; i < size; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar(35);
-			}
+			_putchar(35);
 		}
-		_putchar('\n');
+		if (i != size - 1)
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
