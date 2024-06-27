94901517a4ba6b5338605061207584ee69e15337
root <root@DESKTOP-5OLP95L>
2024-06-27 19:27:59 +0300

    added function to check if char is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isdigit- checks if parameter is a digit.
+* @c: input character.
+* Return: 1 if is an digit character, 0 in other case.
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
