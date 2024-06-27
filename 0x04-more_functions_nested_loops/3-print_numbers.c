f9904aeae0fcf2b62bd361136964fd400260a069
root <root@DESKTOP-5OLP95L>
2024-06-27 20:03:27 +0300

    added function to prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers- print numbers from 0 to 9.
+*
+* Return: 0 for success.
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
