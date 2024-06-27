7ba7f47ae806e209165180976a5d656fbf30a621
root <root@DESKTOP-5OLP95L>
2024-06-27 19:14:00 +0300

    added unction to check uppercase char

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper- returns the upper char
+* @c: the char
+* Return: largest number
+*/
+int _isupper(int c)
+{
+	if (c >= 65 || c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
