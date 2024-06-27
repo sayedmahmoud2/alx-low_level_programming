b2bdeffe7b3e430fc2b6f52b3c1068ed0dfe66a2
root <root@DESKTOP-5OLP95L>
2024-06-27 23:06:06 +0300

    added function to prints straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints staright line
+ * @n: input parmetar
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
