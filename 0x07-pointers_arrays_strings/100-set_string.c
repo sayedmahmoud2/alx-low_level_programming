8d97bd4f67a74522e71fde3f1390bf23d9c9bbba
root <root@DESKTOP-5OLP95L>
2024-07-09 04:52:24 +0300

    add 1 functions

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double adress.
+ * @to: single adress.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
