bb49c7e133b13a716b4f61763600a32638902688
root <root@DESKTOP-5OLP95L>
2024-07-10 13:39:09 +0300

    add 2 function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ *  _power - returns the natural square root of a number.
+ * @n: input number.
+ * @input: iterator.
+ * Return: square root or -1.
+ */
+
+int _power(int n, int input)
+{
+	if (input * input == n)
+		return (input);
+	else if (input * input < n)
+		return (square(n, input + 1));
+	else
+		return (-1);
+}
