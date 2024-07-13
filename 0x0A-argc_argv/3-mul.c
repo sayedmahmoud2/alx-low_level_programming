9b3d73b4e0554ab9c24ba6f34fbdaf85ac052357
root <root@DESKTOP-5OLP95L>
2024-07-13 19:54:48 +0300

    added 2 files 3-mul.c 2-args.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	int result =  atoi(argv[1]) + atoi(argv[2]);
+	int result =  atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", result);
 
