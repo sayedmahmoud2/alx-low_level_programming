e2dbae99f3794f18a517a9c75f2f9b71a2f60d09
root <root@DESKTOP-5OLP95L>
2024-07-13 21:05:42 +0300

    added 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
 }
