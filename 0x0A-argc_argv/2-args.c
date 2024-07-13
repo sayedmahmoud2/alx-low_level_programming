d7c7d91e8132d34cdbd166f384d4f58b9172b6a2
root <root@DESKTOP-5OLP95L>
2024-07-13 19:51:05 +0300

    added 2 files 3-mul.c 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argv; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
