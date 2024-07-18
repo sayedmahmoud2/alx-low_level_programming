a36cbd81dd9a858ceb527a42df92aa6e08b95250
root <root@DESKTOP-5OLP95L>
2024-07-19 00:56:02 +0300

    add 4 files 3-array_range.c 2-calloc.c 1-string_nconcat.c 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
+ * Return: pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *n;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	n = malloc(nmemb * size);
+
+	if (n == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		n[i] = 0;
+
+	return (n);
+}
